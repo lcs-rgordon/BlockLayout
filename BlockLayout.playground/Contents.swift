8f6a999f2ccdc7e1b14b8daecf939fe73fd83ea8
Russell Gordon <rgordon@lcs.on.ca>
2024-09-26 07:19:16 -0400

    Initial Commit

---
+++ BlockLayout.playground/Contents.swift
@@ -0,0 +1,14 @@
+import PlaygroundSupport
+import SwiftUI
+
+struct FirstLayout: View {
+    
+    var body: some View {
+        Phone {
+
+        }
+    }
+    
+}
+
+PlaygroundPage.current.setLiveView(FirstLayout())
